0acd83bc7eb9585583c02834c780df4b57fd2d8a
Anne Schuth <anne.schuth@rijksoverheid.nl>
2026-01-06 11:07:13 +0100

    chore: add knip config and remove unused Slack barrel file
    - Add knip.json for dead code analysis configuration
    - Remove unused src/platform/slack/index.ts (imports go directly to files)

--- src/platform/slack/index.ts
+++
@@ -1,4 +0,0 @@
-export { SlackClient } from './client.js';
-export { SlackFormatter } from './formatter.js';
-export { createSlackPermissionApi, type SlackPermissionApiConfig } from './permission-api.js';
-export * from './types.js';
